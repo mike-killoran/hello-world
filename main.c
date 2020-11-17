90ea864219f9a57e50cb4bc49e569d2aaeaa649e
Mike Killoran <30812439+mike-killoran@users.noreply.github.com>
2020-11-17 16:57:44 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main( int argc, const char* argv[] )
+{
+    int x;
+    int y;
+	
+	// ensure the correct number of parameters are used.
+	if ( argc == 3 ) 
+	{
+		x = atoi( argv[1] );
+		y = atoi( argv[2] );
+		
+		printf( "%d + %d = %d\n", x, y, x + y );
+		
+		// Will print something like: 3 + 2 = 5
+	}
+
+    four spaces
+  a tab
+    two tabs
+
+
+}
